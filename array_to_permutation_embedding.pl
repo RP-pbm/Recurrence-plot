916a73d6c612c92bb73d77a74071862b1fa9b9fc
lynx <recurrence.pbm@gmail.com>
2021-06-24 20:48:20 +0300

    array_to_permutation_embedding.pl: +normalized, +wide_normalized...

--- array_to_permutation_embedding.pl
+++ array_to_permutation_embedding.pl
@@ -16,10 +16,25 @@ my $split = " ";
 my $join = " ";
 my $length;
 my $m = 3;
+my $simple_perm = 1;
+my $simple_norm = 0;
+my $wide_perm = 0;
+my $wide_norm = 0;
 
 for( @opt ){
 	/-m(\d+)/ and do {
 		$m = $1;
+	};
+	/-simple-perm/ and do {
+		$simple_perm = 1;
+	};
+	/-simple-norm/ and do {
+		$simple_perm = 0;
+		$simple_norm = 1;
+	};
+	/-wide-norm/ and do {
+		$simple_perm = 0;
+		$wide_norm = 1;
 	};
 	/-tsv/ and do {
 		$split = "\t";
@@ -62,17 +77,22 @@ for( @FILES ){
 	
 	$debug and print "@{ $_ }\n" for @data;
 	
-	my $length = @{ $data[ 0 ] };
+	if( @data > 1 ){
+		die "$0: Implemented to process only one line (array). $!\n";
+		}
 	
-	my $i = 0;
-	$i = 0, @{ $_ } = map { [ $_, $i ++ ] } @{ $_ } for @data;
+	my @array = @{ $data[ 0 ] };
+	my $length = @array;
 	
 	my @result;
 	push @result, [] for 1 .. $m;
-
-	for my $ref_line ( @data ){
-		for my $i ( 0 .. @{ $ref_line } - $m - 0 ){
-			my @X_refs = @{ $ref_line }[ map { $i + $_ } 0 .. $m - 1 ];
+	
+	if( $simple_perm ){
+		my $i = 0;
+		@array = map { [ $_, $i ++ ] } @array;
+		
+		for my $i ( 0 .. @array - $m - 0 ){
+			my @X_refs = @array[ map { $i + $_ } 0 .. $m - 1 ];
 			$debug and print map "$_\n", join '|', map { join ' ', @{ $_ } } @X_refs;
 			
 			my @pi = map $_->[ 1 ], sort { $a->[ 0 ] <=> $b->[ 0 ] } @X_refs;
@@ -85,8 +105,85 @@ for( @FILES ){
 				push @{ $result[ $j ] }, $pi[ $j ];
 				}
 			}
-		
-		print map "$_\n", join $join, @{ $_ } for @result;
 		}
+	elsif( $simple_norm ){
+		for my $i ( 0 .. @array - $m - 0 ){
+			my @X_refs = @array[ map { $i + $_ } 0 .. $m - 1 ];
+			$debug and print "@X_refs\n";
+			
+			my( $min, $max ) = ( sort { $a <=> $b } @X_refs )[ 0, -1 ];
+			my $diff = $max - $min;
+			
+			@X_refs = map { ( $_ - $min ) / $diff } @X_refs;
+			$debug and print "@X_refs\n";
+			
+			for my $j ( 0 .. @X_refs - 1 ){
+				push @{ $result[ $j ] }, $X_refs[ $j ];
+				}
+			}
+		}
+	elsif( $wide_norm or $wide_perm ){
+		if( $m % 2 == 0 ){
+			die "$0: Implemented for odd m values (but got $m). $!\n";
+			}
+		my $hand = int $m / 2;
+		
+		my %extremum_idxs;
+		for my $i ( 1 .. @array - 2 ){
+			if( $array[ $i - 1 ] < $array[ $i ] and $array[ $i ] > $array[ $i + 1 ] or
+				$array[ $i - 1 ] > $array[ $i ] and $array[ $i ] < $array[ $i + 1 ]
+				){
+				$extremum_idxs{ $i } ++;
+				}
+			}
+		
+		for my $i ( 0 .. @array - 1 ){
+			$array[ $i ] = [ $array[ $i ], exists $extremum_idxs{ $i } ? 1 : 0 ];
+			}
+		
+		for my $i ( 0 .. @array - 1 ){
+			my @L_values;
+			my @R_values;
+			
+			for my $L ( reverse 0 .. $i - 1 ){
+				last if @L_values == $hand;
+				if( $array[ $L ][ 1 ] == 1 ){
+					unshift @L_values, $array[ $L ][ 0 ];
+					}
+				}
+			next if @L_values < $hand;
+			
+			for my $R ( $i + 1 .. @array - 1 ){
+				last if @R_values == $hand;
+				if( $array[ $R ][ 1 ] == 1 ){
+					push @R_values, $array[ $R ][ 0 ];
+					}
+				}
+			next if @R_values < $hand;
+			
+			$debug and print "[$i],L:[@L_values], $array[ $i ][0], R:[@R_values]\n";
+			
+			my @values = ( @L_values, $array[ $i ][ 0 ], @R_values );
+			
+			if( $wide_norm ){
+				my( $min, $max ) = ( sort { $a <=> $b } @values )[ 0, -1 ];
+				my $diff = $max - $min;
+				
+				@values = map { ( $_ - $min ) / $diff } @values;
+				$debug and print "@values\n";
+				
+				for my $j ( 0 .. @values - 1 ){
+					push @{ $result[ $j ] }, $values[ $j ];
+					}
+				}
+			elsif( $wide_perm ){
+				die "$0: To be implemented... $!\n";
+				;;;
+				}
+			}
+		
+		}
+	
+	print map "$_\n", join $join, @{ $_ } for @result;
 }
 
